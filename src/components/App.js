2514464a25c17bdb1b43f9a1645773d850e7a659
satyamjha <satyamjhaactor07@gmail.com>
2025-03-01 18:55:53 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+const Message = () => {
+  return <p>I am learning React. My life is getting better.</p>;
+};
 
-
-export default App;
+export default Message;
